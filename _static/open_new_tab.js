aa2f3543e36f086bb2acaa125d930053a8d9dd8e
PatrickJHess <phess@ucsg.com>
2026-02-19 15:14:04 -0600

    Update documentation

--- _static/open_new_tab.js
+++ _static/open_new_tab.js
@@ -1,24 +1,21 @@
 // _static/open_new_tab.js
-document.addEventListener("DOMContentLoaded", function() {
-    var currentPath = window.location.pathname;
-    
-    // Grab the repo name from the URL if we are on the web
-    // On local 'file:///', this might be different, but that's okay!
-    var currentRepo = currentPath.split('/').filter(Boolean)[0];
+(function() {
+    "use strict";
+    window.addEventListener('load', function() {
+        // Grab the repo name (e.g., "Financial_Python")
+        var currentRepo = window.location.pathname.split('/').filter(Boolean)[0];
 
-    document.querySelectorAll('a').forEach(function(link) {
-        if (link.hostname) {
-            var isExternal = link.hostname !== window.location.hostname;
-            
-            // On GitHub, check if the link's repo matches our current repo
-            var linkRepo = link.pathname.split('/').filter(Boolean)[0];
-            var isDifferentRepo = (linkRepo !== currentRepo);
-
-            // If we are on 'file:///', hostname is empty, so everything with a hostname is "external"
-            if (isExternal || (window.location.protocol !== 'file:' && isDifferentRepo)) {
+        document.querySelectorAll('a').forEach(function(link) {
+            if (link.hostname && link.hostname === window.location.hostname) {
+                var linkRepo = link.pathname.split('/').filter(Boolean)[0];
+                // New tab if the repo name in the URL is different
+                if (linkRepo && linkRepo !== currentRepo) {
+                    link.target = '_blank';
+                }
+            } else if (link.hostname && link.hostname !== window.location.hostname) {
+                // External links (Google, etc)
                 link.target = '_blank';
-                link.rel = 'noopener noreferrer';
             }
-        }
+        });
     });
-});
+})();
