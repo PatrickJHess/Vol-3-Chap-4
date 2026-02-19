8664e90a0f5a93ec8aebf3ad84530a209ee38cda
PatrickJHess <phess@ucsg.com>
2026-02-19 14:38:07 -0600

    Update documentation

--- _static/open_new_tab.js
+++ _static/open_new_tab.js
@@ -1,11 +1,24 @@
 // _static/open_new_tab.js
 document.addEventListener("DOMContentLoaded", function() {
-    var links = document.links;
-    for (var i = 0; i < links.length; i++) {
-        // If the link is not on the same domain as the current page
-        if (links[i].hostname != window.location.hostname) {
-            links[i].target = '_blank';
-            links[i].rel = 'noopener noreferrer';
+    var currentPath = window.location.pathname;
+    
+    // Grab the repo name from the URL if we are on the web
+    // On local 'file:///', this might be different, but that's okay!
+    var currentRepo = currentPath.split('/').filter(Boolean)[0];
+
+    document.querySelectorAll('a').forEach(function(link) {
+        if (link.hostname) {
+            var isExternal = link.hostname !== window.location.hostname;
+            
+            // On GitHub, check if the link's repo matches our current repo
+            var linkRepo = link.pathname.split('/').filter(Boolean)[0];
+            var isDifferentRepo = (linkRepo !== currentRepo);
+
+            // If we are on 'file:///', hostname is empty, so everything with a hostname is "external"
+            if (isExternal || (window.location.protocol !== 'file:' && isDifferentRepo)) {
+                link.target = '_blank';
+                link.rel = 'noopener noreferrer';
+            }
         }
-    }
+    });
 });
